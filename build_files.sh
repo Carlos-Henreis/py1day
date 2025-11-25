d6eb665e8f5c024ece098acdbcf979d3a5a58c65
Carlos-Henreis <carlos_henreis@yahoo.com.br>
2025-11-24 21:54:40 -0300

    Atualizando build

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
+echo "==> Criando ambiente virtual..."
+python3 -m venv .venv
+
+echo "==> Ativando ambiente virtual..."
+source .venv/bin/activate
+
+echo "==> Instalando dependências..."
+pip install --upgrade pip
 pip install -r requirements.txt
-mkdocs build
+
+echo "==> Construindo site..."
+mkdocs build --clean --site-dir site
+
+echo "==> Finalizado!"
